2849139fdea20ddeda0a839f430c8fc6b1a50c77
TinMiner <30262160+TinMiner@users.noreply.github.com>
2019-03-08 16:00:07 +0000

    initial commit

---
+++ build/precache-manifest.c7b4828c041b8d363b66907aead6cd6b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "d5ad63385a7397fc297e",
+    "url": "/static/js/main.75943da2.chunk.js"
+  },
+  {
+    "revision": "29a0d68dc9f27e74e149",
+    "url": "/static/js/2.a8c93675.chunk.js"
+  },
+  {
+    "revision": "d5ad63385a7397fc297e",
+    "url": "/static/css/main.fe60bfc2.chunk.css"
+  },
+  {
+    "revision": "3096af8c904a6e88c221b01c04e05964",
+    "url": "/index.html"
+  }
+];
--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Hotels from './data/hotels.json';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      hotelResults : [],
+      searchStr : 'pool',
+    };
+
+
+    this.handleSearch = this.handleSearch.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSearch(event) {
+
+    this.setState({ searchStr: event.target.value });
+    this.state.hotelResults = [];
+	//this.setState({ hotelResults: [] });
+
+    //add locations to search results
+    if( this.state.searchStr.length > 1){
+      for(let i=0; i < Hotels.length; i++){
+        for(let j=0; j< Hotels[i].facilities.length; j++){
+          if( Hotels[i].facilities[j].toLowerCase().indexOf(this.state.searchStr.toLowerCase()) !== -1){   
+            //check if room is already in the result
+            if( !this.state.hotelResults.includes(Hotels[i].name))
+              this.state.hotelResults.push(Hotels[i].name);
+          }
+        }
+      }
+    }
+
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div style={{ textAlign: "center" }}>
+        
+        <form onSubmit={this.handleSubmit}>
+          Search:
+          <input type="text" value={this.state.searchStr} onChange={this.handleSearch} />
+        </form>
+
+        <div style={{ display: "inline-block", width: 200, position: "absolute", marginLeft: -100, backgroundColor: "yellow"}}>
+        {
+          //based upon search result locations display the facilities at the hotel
+		
+            Hotels.map( (item, uniqueID) => {        
+            for(let i=0; i < this.state.hotelResults.length; i++){
+              if(this.state.hotelResults[i] === item.name ){
+                return(
+                  <ul><li key={item.id} style={{ textAlign: "center" }}><b>{item.name}</b><ul>
+                  {
+                    item.facilities.map( (subitem, index ) => <li key={index}>{subitem}</li> ) 
+                  }
+                  </ul></li></ul>
+                );  
+              }		  
+            }
+		 })
+        }
+        </div>
+
       </div>
     );
   }
